54430a6f0795e913072354e4520b417355288ccd
srijan0412 <95132945+srijan0412@users.noreply.github.com>
2023-04-10 22:35:31 +0530

    Hero section complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import {Tilt} from 'react-tilt';
+import {motion} from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import {fadeIn, textVariant } from '../utils/motion';
 
-const About = () => {
+const ServiceCard = (index, title, icon) => {
   return (
-    <div>About</div>
+    <p>{title}</p>
   )
 }
 
-export default About
+
+const About = () => {
+  return (
+    <>
+    <motion.div variants={textVariant}>
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview.</h2>
+    </motion.div>
+    <motion.p variants={fadeIn("", "", 0.1, 1)}
+      className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+    >
+      I'm a skilled software developer with experience in Typescript and 
+      Javascript, and expertise in frameworks like React, Node.js, and Three.js.
+      I'm a quick learner and collaborate closely with cliendts to 
+      crate efficient, scalable, and user-friendly solutions that solve real-world problems.
+      Let's work together to bring your ideas to life!
+    </motion.p>
+    <div className='mt-20 flex flex-wrap gap-10'>
+      {services.map((service, index) => {
+        <ServiceCard key={services.title}
+        index={index} {...service}/>
+      })}
+    </div>
+    </>
+  )
+}
+
+export default About;
