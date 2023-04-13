d4b83c88d928b96876a80725f1fd19f9797af6ec
srijan0412 <95132945+srijan0412@users.noreply.github.com>
2023-04-13 21:19:54 +0530

    feedback-section completed

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -4,6 +4,47 @@ import { SectionWrapper } from '../hoc';
 import { fadeIn, textVariant } from '../utils/motion';
 import { testimonials } from '../constants';
 
+const FeedbackCard = ({index, testimonial, name, designation, company, image}) => (
+  <motion.div
+  variants={fadeIn("", "spring", index*0.5, 0.75)}
+  className='bg-black p-10 rounded-3xl xs:w-[320px] w-full'
+  >
+    <p
+      className='text-white font-black text-[48px]'
+    >"</p>
+    <div className="mt-1">
+      <p
+        className='text-white tracking-wider text-[18px]'
+      >{testimonial}</p>
+      <div
+        className='mt-7 flex justify-between gap-1'
+      >
+        <div
+          className='flex-1 flex flex-col'
+        >
+          <p
+            className='text-white font-medium text-[16px]'
+          >
+            <span
+              className='blue-text-gradient'
+            >@</span>{name}
+          </p>
+          <p 
+            className='mt-1 text-secondary text-[12px]'
+          >
+            {designation} of {company}
+          </p>
+        </div>
+
+        <img 
+          src={image} 
+          alt={`feedbaack-by-${name}`}
+          className='w-10 h-19 rounded-full object-cover' 
+        />
+      </div>
+    </div>
+  </motion.div>
+)
 
 const Feedbacks = () => {
   return (
@@ -25,8 +66,19 @@ const Feedbacks = () => {
           >Testimonials.</h2>
         </motion.div>
       </div>
+      <div
+        className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}
+      >
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard 
+            key={testimonial.name}
+            index={index}
+            {...testimonial}
+          />
+        ))}
+      </div>
     </div>
   )
 }
 
-export default Feedbacks
+export default SectionWrapper(Feedbacks, ""); 
